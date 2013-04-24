8b04531e19aa8dea15bccf6a9de2c649c31b8d24
lanqiyi <zhaomin0923@gmail.com>
2013-04-24 23:18:05 +0800

    format

--- src/test/hello.java
+++ src/test/hello.java
@@ -2,21 +2,23 @@ package test;
 import test.I_hello;
 import test.hello1;
 
-
-interface I_hello{
+interface I_hello {
 	void HelloWorld();
 }
-class hello1 implements  I_hello{
-	public void HelloWorld(){
+
+class hello1 implements I_hello {
+	public void HelloWorld() {
 		System.out.println("Hello World!");
 	}
-	 public void xx(){
-		 System.out.println("I'm good!");
-	 }
+
+	public void xx() {
+		System.out.println("I'm good!");
+	}
 }
+
 public class hello {
-	public static void main(String args[]){
-		hello1 myfunction=new hello1();
+	public static void main(String args[]) {
+		hello1 myfunction = new hello1();
 		myfunction.HelloWorld();
 	}
 }
